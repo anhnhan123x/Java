4bf0d101f1b59f182821d92d92ada006ace22b33
anhnhan123x <letritrung2016@gmail.com>
2024-12-25 02:50:25 +0700

    first java lession

---
+++ src/TrungTH/HelloWorld.java
@@ -0,0 +1,10 @@
+package TrungTH;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello World with Trung");
+		System.out.println("hello World with Trung");
+		System.out.println("hello World with Trung");
+		System.out.println("hello World with Trung");
+	}
+}
